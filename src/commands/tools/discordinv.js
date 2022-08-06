8077a60909e28fb1f9f08368a04653b912b0a1cd
Ciochinio <48630580+Ciochinio@users.noreply.github.com>
2022-08-06 13:01:52 +0200

    commands

--- src/commands/tools/discordinv.js
+++ src/commands/tools/discordinv.js
@@ -2,14 +2,14 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('launcher')
-        .setDescription('Launcher do łatwego wchodzenia na WitcherCrafta'),
+        .setName('discord')
+        .setDescription('Discord RedBoltMedia'),
     async execute(interaction, client){
         const message = await interaction.deferReply({
             fetchReply: true
         });
 
-        const newMessage = `https://redboltmedia.net/download/witchercraft-installer.exe`
+        const newMessage = `Zapraszam serdecznie na discord RedBoltMedia: https://discord.gg/xReJfktHS2 `
         await interaction.editReply({
             content:newMessage
         });
